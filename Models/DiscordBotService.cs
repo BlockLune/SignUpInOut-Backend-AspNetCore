dd51c021b5bdef8adb95f0e561abd8a31ca66abd
BlockLune <39331194+BlockLune@users.noreply.github.com>
2024-07-14 16:13:49 +0800

    feat: discord bot starter

---
+++ Models/DiscordBotService.cs
@@ -0,0 +1,41 @@
+﻿using Discord;
+using Discord.WebSocket;
+
+namespace SignUpInOut_Backend_AspNetCore.Models
+{
+    public class DiscordBotService
+    {
+        private readonly DiscordSocketClient _client;
+        private readonly string _token;
+
+        public DiscordBotService(DiscordSocketClient client)
+        {
+            _client = client;
+            _client.Log += LogAsync;
+            _client.MessageReceived += MessageReceivedAsync;
+
+            _token = Environment.GetEnvironmentVariable("SIGNUPINOUT_DISCORD_BOT_TOKEN") ?? "";
+        }
+
+        public async Task InitializeAsync()
+        {
+            await _client.LoginAsync(TokenType.Bot, _token);
+            await _client.StartAsync();
+        }
+
+        private Task LogAsync(LogMessage msg)
+        {
+            Console.WriteLine(msg.ToString());
+            return Task.CompletedTask;
+        }
+
+        private async Task MessageReceivedAsync(SocketMessage messageParam)
+        {
+            if (messageParam.Author.IsBot) return;
+            if (messageParam.Content == "您好")
+            {
+                await messageParam.Channel.SendMessageAsync("您也好呀");
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,10 +1,19 @@
 using Hei.Captcha;
 using Microsoft.EntityFrameworkCore;
 using SignUpInOut_Backend_AspNetCore.Models;
+using DotNetEnv;
+using Discord;
+using Discord.WebSocket;
+
+Env.Load();
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+// discord bot
+builder.Services.AddSingleton<DiscordSocketClient>();
+builder.Services.AddSingleton<DiscordBotService>();
+
 // captcha
 builder.Services.AddSingleton<SecurityCodeHelper>(); // 注册 SecurityCodeHelper 为单例
 builder.Services.AddScoped<CaptchaCache>(); // 注册 CaptchaCache 为作用域范围的服务
@@ -33,6 +42,9 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+var botService = app.Services.GetRequiredService<DiscordBotService>();
+await botService.InitializeAsync();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
